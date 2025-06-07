16a9825467b5b856cfdfadcda77dee46d6816261
Bij <tomas-bijecek@seznam.cz>
2025-06-07 12:15:08 +0200

    Neural Network logic split into multiple files

---
+++ src/NeuralNetwork.cu
@@ -0,0 +1,133 @@
+#include <cudaDefs.h>
+#include <random>
+#include <algorithm>
+#include <iostream>
+#include "NeuralNetwork.cuh"
+#include "ActivationFunctions.cuh"
+
+using std::cout;
+using std::endl;
+
+
+void setNumSamplesConstant(int input_size) {
+	checkCudaErrors(cudaMemcpyToSymbol((const void*)&num_samples, &input_size, sizeof(int)));
+}
+
+// Jedno vlákno = jeden sample - jeden neuron
+__global__ void forward(const float* __restrict__ input_data, int input_size, const float* __restrict__ weight_matrix, const float* __restrict__ bias, float* __restrict__ output_data, int output_size, bool compute_relu) {
+	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
+	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (sample_id < num_samples && neuron_id < output_size) {
+		float sum = 0.0;
+
+		for (int i = 0; i < input_size; i++) {
+			sum += input_data[sample_id * input_size + i] * weight_matrix[neuron_id * input_size + i];
+		}
+
+		// Pøidej bias (input 0,0 -> target 0 by jinak nefungovalo)
+		sum += bias[neuron_id];
+
+		if (compute_relu) {
+			output_data[sample_id * output_size + neuron_id] = convert_relu(sum);
+		}
+		else {
+			output_data[sample_id * output_size + neuron_id] = convert_sigmoid(sum);
+		}
+
+	}
+}
+
+//256 velikost -> pracuju s 128 THREADS_PER_BLOCK
+__global__ void compute_loss(float* y_predicted, float* y_true, float* loss, int size) {
+	__shared__ float s_loss[THREADS_PER_BLOCK];
+
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	int next = size / 2;
+
+	float epsilon = 1e-7;
+
+	// TODO SIZE/2
+	if (idx < size) {
+		s_loss[idx] = -y_true[idx] * logf(fmax(y_predicted[idx], epsilon));
+		__syncthreads();
+
+		if (idx + next > size) {
+			return;
+		}
+
+		while (next > 0) {
+			s_loss[idx] += s_loss[idx + next];
+
+			__syncthreads();
+
+			next >>= 1;
+
+			if (idx > next) {
+				return;
+			}
+		}
+		if (idx == 0) {
+			loss[0] = s_loss[0];
+		}
+	}
+}
+
+__global__ void compute_gradient(float* y_pred, float* y_true, float* gradient, int size) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (idx < size) {
+		gradient[idx] = y_pred[idx] - y_true[idx];
+	}
+}
+__global__ void backward(float* input, float* activations, int input_size, float* weight_matrix, bool first, float* gradient_in
+	, float* gradient_out, int output_size, int next_out_size) {
+
+	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
+	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (sample_id < num_samples && neuron_id < output_size) {
+
+		if (first) {
+			gradient_out[sample_id * output_size + neuron_id] = input[sample_id * output_size + neuron_id] * derivate_sigmoid(activations[sample_id * output_size + neuron_id]);
+		}
+		else {
+
+			float sum = 0.0;
+			for (int j = 0; j < next_out_size; j++) {
+				sum += weight_matrix[j * output_size + neuron_id] * gradient_in[sample_id * next_out_size + j];
+			}
+
+			gradient_out[sample_id * output_size + neuron_id] = sum * derivate_relu(activations[sample_id * output_size + neuron_id]);
+
+		}
+	}
+}
+// Funkce pro aktualizaci vah a biasu
+// TODO:
+//      Pøepoèítat si zmìnu váhy pro N vzorkù a až potom volat tuhle funkci
+__global__ void update_parameters(float* input, float* gradient, float* weight_matrix, float* biases, int input_size, int output_size) {
+	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
+	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (sample_id < num_samples) {
+		// Learning rate
+		float learning_rate = 0.1f;
+
+
+		// Get the gradient for the current sample and output neuron
+		float grad = gradient[sample_id * output_size + neuron_id];
+
+		// Update each weight for this output neuron
+		for (int i = 0; i < input_size; i++) {
+			// weight_matrix is [output_size][input_size]
+			atomicAdd(&weight_matrix[neuron_id * input_size + i], -learning_rate * input[sample_id * input_size + i] * grad);
+
+		}
+
+		// Update bias
+		atomicAdd(&biases[neuron_id], -learning_rate * grad);
+
+	}
+}
--- src/Runner.cu
+++ src/Runner.cu
@@ -3,172 +3,18 @@
 #include <algorithm>
 #include <iostream>
 
+#include "NeuralNetwork.cuh"
+#include "Layer.cuh"
+#include "ActivationFunctions.cuh"
+#include "Datasets.cuh"
+
 using std::cout;
 using std::endl;
 
-constexpr unsigned int THREADS_PER_BLOCK = 128;
+//constexpr unsigned int THREADS_PER_BLOCK = 128;
 
 cudaError_t error = cudaSuccess;
 cudaDeviceProp deviceProp = cudaDeviceProp();
-
-__constant__ int num_samples;
-
-struct Layer {
-	int in;
-
-	int out;
-	// 2D matice jako 1D pole
-	float* weights;
-
-	// 1D matice
-	float* biases;
-
-	//float* pre_activations;
-		
-	float* activations;
-
-	float* gradients;
-};
-
-// Pomocná funkce pro výpočet RELU
-__device__ float convert_relu(float sum) {
-	return sum > 0.0 ? sum : 0.0;
-}
-
-// Pomocná funkce pro výpočet SIGMOID
-__device__ float convert_sigmoid(float sum) {
-	return 1.0f / (1.0f + expf(-sum));
-}
-
-// Jedno vlákno = jeden sample - jeden neuron
-__global__ void forward(const float* __restrict__ input_data, int input_size, const float* __restrict__ weight_matrix, const float* __restrict__ bias, float* __restrict__ output_data, int output_size, bool compute_relu) {
-	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
-	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if (sample_id < num_samples && neuron_id < output_size) {
-		float sum = 0.0;
-
-		for (int i = 0; i < input_size; i++) {
-			sum += input_data[sample_id * input_size + i] * weight_matrix[neuron_id * input_size + i];
-		}
-
-		// Přidej bias (input 0,0 -> target 0 by jinak nefungovalo)
-		sum += bias[neuron_id];
-
-		if (compute_relu) {
-			output_data[sample_id * output_size + neuron_id] = convert_relu(sum);
-		}
-		else {
-			output_data[sample_id * output_size + neuron_id] = convert_sigmoid(sum);
-		}
-		
-	}
-}
-
-//256 velikost -> pracuju s 128 THREADS_PER_BLOCK
-__global__ void compute_loss(float* y_predicted, float* y_true, float* loss, int size) {
-	__shared__ float s_loss[THREADS_PER_BLOCK];
-	
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	int next = size/2;
-
-	float epsilon = 1e-7;
-	
-	// TODO SIZE/2
-	if (idx < size) {
-		s_loss[idx] = -y_true[idx] * logf(fmax(y_predicted[idx], epsilon));
-		__syncthreads();
-
-		if (idx + next > size) {
-			return;
-		}
-
-		while (next > 0) {
-			s_loss[idx] += s_loss[idx + next];
-
-			__syncthreads();
-
-			next >>= 1;
-
-			if (idx > next) {
-				return;
-			}
-		}
-		if (idx == 0) {
-			loss[0] = s_loss[0];
-		}
-	}
-}
-
-__global__ void compute_gradient(float* y_pred, float* y_true, float* gradient, int size) {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (idx < size) {
-		gradient[idx] = y_pred[idx] - y_true[idx];
-	}
-}
-
-// Pomocná funkce pro derivaci RELU
-__device__ float derivate_relu(float sum) {
-	return sum > 0 ? 1.0 : 0.0;
-}
-
-// Pomocná funkce pro derivaci SIGMOID
-__device__ float derivate_sigmoid(float sum) {
-	return sum * (1.0f - sum);
-}
-
-__global__ void backward(float* input, float* activations, int input_size, float* weight_matrix, bool first, float* gradient_in
-	, float* gradient_out, int output_size, int next_out_size) {
-	
-	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
-	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if (sample_id < num_samples && neuron_id < output_size) {
-
-		if (first) {
-			gradient_out[sample_id * output_size + neuron_id] = input[sample_id * output_size + neuron_id] * derivate_sigmoid(activations[sample_id * output_size + neuron_id]);
-		}
-		else {
-			
-			float sum = 0.0;
-			for (int j = 0; j < next_out_size; j++) {
-				sum += weight_matrix[j * output_size + neuron_id] * gradient_in[sample_id * next_out_size + j];
-			}
-
-			gradient_out[sample_id * output_size + neuron_id] = sum * derivate_relu(activations[sample_id * output_size + neuron_id]);
-			
-		}
-	}
-}
-// Funkce pro aktualizaci vah a biasu
-// TODO:
-//      Přepočítat si změnu váhy pro N vzorků a až potom volat tuhle funkci
-__global__ void update_parameters(float* input, float* gradient, float* weight_matrix, float* biases, int input_size, int output_size) {
-	int sample_id = blockIdx.x * blockDim.x + threadIdx.x;
-	int neuron_id = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if (sample_id < num_samples) {
-		// Learning rate
-		float learning_rate = 0.1f;
-
-		
-		// Get the gradient for the current sample and output neuron
-		float grad = gradient[sample_id * output_size + neuron_id];
-
-		// Update each weight for this output neuron
-		for (int i = 0; i < input_size; i++) {
-			// weight_matrix is [output_size][input_size]
-			atomicAdd(&weight_matrix[neuron_id * input_size + i], -learning_rate * input[sample_id * input_size + i] * grad);
-
-		}
-
-		// Update bias
-		atomicAdd(&biases[neuron_id], -learning_rate * grad);
-		
-	}
-}
 
 
 int main(int argc, char* argv[])
@@ -189,77 +35,62 @@ int main(int argc, char* argv[])
 	const int num_hidden = 1;
 	// Počet iterací tréninku
 	const int n_of_iterations = 50;
-	// TODO: learning rate jako konstant memory
-	//const float lr = 0.01f;
 
-	// Nahodny generator
-	std::mt19937 gen(42);
-	std::uniform_real_distribution<float> dist(-1.0, 1.0f);
 
+
+//******************************************************************************************|
+//								    INPUT DATASETS										    |						
+//******************************************************************************************|
+
+	Dataset ds = getDatasetByName("dataset1");
+
+	const std::vector<float> X = ds.input;
+	const std::vector<float> y = ds.target;
+
+
+//******************************************************************************************|
+//							   INPUT DATA ALLOCATION ON GPU									|						
+//******************************************************************************************|
+
+	float* d_input;
+	checkCudaErrors(cudaMalloc(&d_input, input_size * input_dimension * sizeof(float)));
+	checkCudaErrors(cudaMemcpy(d_input, X.data(), input_size * input_dimension * sizeof(float), cudaMemcpyHostToDevice));
+
+	float* d_target;
+	checkCudaErrors(cudaMalloc(&d_target, output_size * output_dimension * sizeof(float)));
+	checkCudaErrors(cudaMemcpy(d_target, y.data(), output_size * output_dimension * sizeof(float), cudaMemcpyHostToDevice));
+
+
+//******************************************************************************************|
+//								      CREATE LAYERS											|						
+//******************************************************************************************|
 
 	std::vector<Layer> layers;
 
 	// Vstupní -> Hidden1
-	layers.push_back({ input_dimension, hidden_size, nullptr, nullptr, nullptr, nullptr });
+	layers.push_back(createDenseLayer(input_dimension, hidden_size, ActivationFunction::RELU));
 
 	// Hidden1 -> HiddenN
 	for (int i = 0; i < num_hidden; i++) {
-		layers.push_back({ hidden_size, hidden_size, nullptr, nullptr, nullptr, nullptr });
+		layers.push_back(createDenseLayer(hidden_size, hidden_size, ActivationFunction::RELU));
 	}
 
 	// HiddenN -> Výstupní
-	layers.push_back({ hidden_size, output_dimension, nullptr, nullptr, nullptr, nullptr });
+	layers.push_back(createDenseLayer(hidden_size, output_dimension, ActivationFunction::SIGMOID));
 
-	float* h_input = new float[input_size * input_dimension]
-		{
-			0, 0, 1, 1, 0, 1, 1, 0
-		};
-	float* h_target = new float[output_size * output_dimension]
-		{
-			0, 0, 1, 1
-		};
-
-
-	// Alokace vrstev
+//******************************************************************************************|
+//								  LAYERS ALLOCATION ON GPU								    |					
+//******************************************************************************************|
+	
 	for (auto &layer : layers) {
-		// Váhová matice
-		checkCudaErrors(cudaMalloc(&layer.weights, layer.in * layer.out * sizeof(float)));
-		// Bias
-		checkCudaErrors(cudaMalloc(&layer.biases, layer.out * sizeof(float)));
-		// Aktivace
-		checkCudaErrors(cudaMalloc(&layer.activations, input_size * layer.out * sizeof(float)));
-		// Gradient
-		checkCudaErrors(cudaMalloc(&layer.gradients, input_size * layer.out * sizeof(float)));
-
-		// Inicializace defaultních hodnot pro váhy a bias
-		// TODO: Generaci přesunout na GPU
-		std::vector<float> temporary_weights(layer.in * layer.out), temporary_biases(layer.out);
-
-		for (auto& v : temporary_weights) v = dist(gen);
-		for (auto& v : temporary_biases) v = dist(gen);
-
-		checkCudaErrors(cudaMemcpy(layer.weights, temporary_weights.data(), layer.in * layer.out * sizeof(float), cudaMemcpyHostToDevice));
-		checkCudaErrors(cudaMemcpy(layer.biases, temporary_biases.data(), layer.out * sizeof(float), cudaMemcpyHostToDevice));
-
-		// LOGOVANI
-		//checkDeviceMatrix<float>(layer.weights, layer.in * layer.out * sizeof(float), 1, layer.in * layer.out, "%f ", "Weights: ");
-
+		initLayer(layer, input_size);
 	}
-
-	float* d_input;
-	checkCudaErrors(cudaMalloc(&d_input, input_size * input_dimension * sizeof(float)));
-	checkCudaErrors(cudaMemcpy(d_input, h_input, input_size * input_dimension * sizeof(float), cudaMemcpyHostToDevice));
-
-	float* d_target;
-	checkCudaErrors(cudaMalloc(&d_target, output_size * output_dimension * sizeof(float)));
-	checkCudaErrors(cudaMemcpy(d_target, h_target, output_size * output_dimension * sizeof(float), cudaMemcpyHostToDevice));
-
 
 	float* d_calculated_loss;
 	checkCudaErrors(cudaMalloc(&d_calculated_loss, sizeof(float)));
 
 
-	checkCudaErrors(cudaMemcpyToSymbol((const void*)&num_samples, &input_size, sizeof(int)));
+	setNumSamplesConstant(input_size);
 
 
 	// Definice loss pole
@@ -278,8 +109,9 @@ int main(int argc, char* argv[])
 	dim3 dimGrid{ 1,1 ,1 };
 
 
-
-	// Hlavní trénovací smyčka
+//******************************************************************************************|
+//								         MAIN TRAINING LOOP						            |					
+//******************************************************************************************|
 	for (int iteration = 0; iteration < n_of_iterations; iteration++) {
 
 		// Resetovani loss
